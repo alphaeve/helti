5c66ddb9c904bd55e8f4aec18e9b4212bf23af14
harshil <devadharaharshil@gmail.com>
2025-08-15 14:14:12 +0530

    fld

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -2,7 +2,9 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import { db } from "../lib/firebase";
+// import { db } from "../lib/firebase";
+import { db } from "../../lib/firebase";
+
 import { doc, getDoc } from "firebase/firestore";
 import { remark } from "remark";
 import html from "remark-html";
